6e34cb5bfe2d62f2969076b29c1d52a809e2ea1e
V0od0oChild <r.miguel.f.sousa@gmail.com>
2025-06-03 17:22:49 +0100

    Adjust to pack without user/group

--- pkgbuild/scripts/batocera-makepkg.sh
+++ pkgbuild/scripts/batocera-makepkg.sh
@@ -120,9 +120,9 @@ function make_pkg() {
 	echo "Creating package $pk ..."
 	LST_BATOEXEC=( $(ls "$BATOEXEC"* 2>/dev/null) )
 	if ! [[ -z "${LST_BATOEXEC[@]}" ]]; then
-	    sudo tar -cf --owner=0 --group=0 --no-same-owner --no-same-permissions - "$INFO" "$BATOEXEC"* * | zstd -c --rsyncable - -o "$pk"
+	    tar --owner=0 --group=0 --numeric-owner --mode=go+rwX --no-acls --no-xattrs -cf - "$INFO" "$BATOEXEC"* * | zstd -c --rsyncable - -o "$pk"
 	else
-	    sudo tar -cf --owner=0 --group=0 --no-same-owner --no-same-permissions - "$INFO" * | zstd -c --rsyncable - -o "$pk"
+	    tar --owner=0 --group=0 --numeric-owner --mode=go+rwX --no-acls --no-xattrs -cf - "$INFO" * | zstd -c --rsyncable - -o "$pk"
 	fi
 	ret=$?
 	chmod go+r "$pk" 2>/dev/null
